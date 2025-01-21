61f769d4374e3c921e5ecd404cdc9b7995f40d8d
Valentina <valentina.lopez02@alumnos.ucn.cl>
2025-01-21 23:39:24 +0000

    new

--- main.cpp
+++ main.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main (){
-    
+    string nombreArchivo;
+    cout << "¿Cuál es el nombre de su archivo?: ";
+    cin >> nombreArchivo;
+
+    ifstream nuevoArchivo(nombreArchivo);
+    if (!nuevoArchivo) {
+        cerr << "¡ERROR! No se ha podido abrir el archivo" << endl;
+        return 1;
+
 }
